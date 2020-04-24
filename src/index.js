48025eb2d0c1e8238eab1fd6fa10078091b447d7
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2020-04-24 10:12:41 -0500

    Challenge solved

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-function maxSequence(array) {
-  // your code
-  return;
+/**
+ * Find the max sum of sub sequence that maximin this sum.
+ * @param {number[]} array 
+ * @returns {number} max sum
+ */
+const maxSequence = (array) => {
+  if (array.length === 0) return 0;
+
+  let partialSums = [
+    array.reduce((pre, cur) => pre + cur),
+    maxSequence(array.slice(1)),
+    maxSequence(array.slice(0, -1))
+  ]
+
+  return partialSums.reduce((pre, cur) => pre < cur ? cur : pre);
 }
 
 module.exports = { maxSequence };
