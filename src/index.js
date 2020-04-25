ac881c86d0f27b7ba42711e6da77cf22d4a2683f
Iván Chipatecua <ivchip@gmail.com>
2020-04-25 00:14:27 -0500

    Challenge JavaScript 15 completed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 function maxSequence(array) {
-  // your code
-  return;
+  let maxSum = 0
+  let tempSum = 0
+
+  for (const item of array) {
+    tempSum += item;
+    maxSum = Math.max(maxSum, tempSum);
+    if (tempSum < 0) tempSum = 0;
+  }
+
+  return maxSum;
 }
 
 module.exports = { maxSequence };
