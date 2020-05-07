cdc26c7b10aec977bd26fad535e8007241079e7e
Cristian F. Tovar <cristhiam.tovar@mail.escuelaing.edu.co>
2020-05-07 18:32:03 -0500

    ready to push

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-(function maxSequence(array) {
+function maxSequence(array) {
+  if (array.length === 0) return 0
+
   const result = [];
   for(j in array){
     let acum = 0;
     for(k=j;k<array.length;k++){
       acum = acum + array[k];
       result.push(acum);
-      console.log(`current acum=${acum}`);      
+     
     }
   }
   const max = Math.max(...result)
@@ -13,16 +15,16 @@
   const ini = Math.round(index/array.length)
   const residual = index%(array.length)
 
-  console.log(`ini=${ini}\nresidual=${residual}`);  
-  console.log(`max=${max}`);
+ 
+ 
 
   const sum = [];
 
   for (let k = ini; k <= residual+1; k++ ){
     sum.push(array[k]);
   }
-  console.log(sum);
-  return;
-})([-2,6,9,9,-7,-3,10])
+ 
+  return max;
+}
 
-// module.exports = { maxSequence };
+module.exports = { maxSequence };
